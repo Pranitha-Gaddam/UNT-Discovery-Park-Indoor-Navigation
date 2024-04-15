911e70e4815d32384032cf6462f4d3bfdf023f27
Pranitha-Gaddam <105523022+Pranitha-Gaddam@users.noreply.github.com>
2024-04-14 22:11:28 -0500

    Add files via upload

---
+++ src/path1.js
@@ -0,0 +1,29 @@
+const path1 = {
+  "type": "FeatureCollection",
+  "name": "path1",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": { "id": 1, "floor": 1 },
+      "geometry": { "type": "LineString", "coordinates": [ [ -97.15387406945061, 33.25485939795162 ], [ -97.15375011751058, 33.25479742198161 ] ] }
+    },
+    {
+      "type": "Feature",
+      "properties": { "id": 2, "floor": 1 },
+      "geometry": { "type": "LineString", "coordinates": [ [ -97.15375011751058, 33.25479742198161 ], [ -97.15366789275183, 33.25491032086014 ] ] }
+    },
+    {
+      "type": "Feature",
+      "properties": { "id": 3, "floor": 1 },
+      "geometry": { "type": "LineString", "coordinates": [ [ -97.15366789275183, 33.25491032086014 ], [ -97.15359717116993, 33.25500830865747 ] ] }
+    },
+    {
+      "type": "Feature",
+      "properties": { "id": 4, "floor": 1 },
+      "geometry": { "type": "LineString", "coordinates": [ [ -97.15375011751058, 33.25479742198161 ], [ -97.15358493105212, 33.25471436857528 ] ] }
+    },
+    { "type": "Feature", "properties": { "id": 5, "floor": 1 }, "geometry": { "type": "LineString", "coordinates": [ [ -97.15358493105212, 33.25471436857528 ], [ -97.15350361689489, 33.25482843924073 ] ] } },
+    { "type": "Feature", "properties": { "id": 6, "floor": 1 }, "geometry": { "type": "LineString", "coordinates": [ [ -97.15350361689489, 33.25482843924073 ], [ -97.15366789275183, 33.25491032086014 ] ] } }
+  ]
+};
+export default path1;
