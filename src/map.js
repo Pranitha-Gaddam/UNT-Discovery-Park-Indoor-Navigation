9f2e8dc0a09535033f7bd55852e6ebbe9948910c
Kim Vo <126284919+KimVVo@users.noreply.github.com>
2024-04-16 22:24:39 -0500

    Update map.js

--- src/map.js
+++ src/map.js
@@ -33,7 +33,19 @@ console.log("end: ", endRoom);
             visualizePitch: true
         });
         map.addControl(nav, 'top-right');
-    
+     // Add geolocate control to the map.
+ map.addControl(
+    new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true
+    }),
+    'bottom-left'
+); 
 
         // Fetch the data.json file
         fetch('data/data.geojson')
