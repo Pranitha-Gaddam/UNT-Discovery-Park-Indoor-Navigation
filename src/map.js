d37433b97d167335df18e4336cfefa4afb77d8b2
LinhTran00 <121794127+LinhTran00@users.noreply.github.com>
2024-04-23 11:49:24 -0500

    Update map.js

--- src/map.js
+++ src/map.js
@@ -43,7 +43,8 @@ console.log("end: ", endRoom);
                  // When active the map will receive updates to the device's location as it changes.
                  trackUserLocation: true,
                  // Draw an arrow next to the location dot to indicate which direction the device is heading.
-                 showUserHeading: true
+                 showUserHeading: true,
+                 zoom : 25
              }),
              'bottom-left'
         ); 
