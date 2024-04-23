a9bbaf73560e772c12d91c75b21dfc3f2bc0b850
Pranitha-Gaddam <105523022+Pranitha-Gaddam@users.noreply.github.com>
2024-04-23 12:19:02 -0500

    Update routing.js

--- src/routing.js
+++ src/routing.js
@@ -114,10 +114,10 @@ function floor1to2(source, f1, destination, f2) {
     turnTypeElement.innerHTML = turnTypeHTML;
     document.getElementById('turnbyturn-dir').appendChild(turnTypeElement);
 
-    let turnTypeHTML = `<div id="eachturn">
+    turnTypeHTML = `<div id="eachturn">
     <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M440-120v-567l-64 63-56-56 160-160 160 160-56 56-64-63v567h-80Z"/></svg>
     Continue straight </div>`;
-    let turnTypeElement = document.createElement('div');
+    turnTypeElement = document.createElement('div');
     turnTypeElement.innerHTML = turnTypeHTML;
     document.getElementById('turnbyturn-dir').appendChild(turnTypeElement);
 
